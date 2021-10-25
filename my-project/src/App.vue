56deb7fae388fb6d7a0902f423a1f79c7ab2caa8
Kirill Trezubov <iiikirill@yandex.ru>
2021-10-25 18:30:51 +0300

    add my-project under git

---
+++ my-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <app-example>{{ message }}</app-example>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ my-project/src/Example.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <app-input :msg="message" @messChange="message = $event"></app-input>
+    </div>
+</template>
+
+<script>
+    import Input from './Field.vue';
+    export default {
+        data() {
+            return {
+                message: 'Пробный текст'
+            }
+        },
+        components: {
+            'app-input': Input
+        }
+    }
+</script>
